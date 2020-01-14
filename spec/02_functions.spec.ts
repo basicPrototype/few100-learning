f605b335f5282062ba85d2338d0b7f54bd0de446
basicPrototype <rmarquis@gmail.com>
2020-01-14 15:54:20 -0500

    higher order functions

--- spec/02_functions.spec.ts
+++ spec/02_functions.spec.ts
@@ -38,6 +38,7 @@ describe('functions', () => {
     describe('arguments to functions', () => {
         it('does not have overloading', () => {
 
+            // overloaded parameters
             function formatName(first: string, last: string, mi?: string) {
                 let firstPart = `${last}, ${first}`;
                 if (mi) {
@@ -49,6 +50,96 @@ describe('functions', () => {
             expect(formatName('Han', 'Solo')).toBe('Solo, Han');
             expect(formatName('Han', 'Solo', 'D')).toBe('Solo, Han D.');
         });
+
+        it('default values for params', () => {
+            function add(a: number = 15, b: number = 10) {
+                return a + b;
+            }
+
+            expect(add(2, 2)).toBe(4);
+            expect(add(12)).toBe(22);
+            expect(add(undefined, 12)).toBe(27); // ONLY undefined keyword can skip a param (and use default)
+        });
+
+        it('using the rest operator...', () => {
+            function add(a: number, b: number, ...rest: number[]) {
+                const firstTwo = a + b;
+                return rest.reduce((s, n) => s + n, firstTwo); // higher order function - reduce takes a function as an argument
+            }
+
+
+            expect(add(2, 2)).toBe(4);
+            expect(add(1, 2, 3, 4, 5, 6, 7, 8, 9)).toBe(45);
+        });
+    });
+
+    describe('higher order functions', () => {
+        // A function that takes one or more arguments that are functions and/or
+        // return a function
+        it('the basic syntax', () => {
+
+            type StringModifier = (msg: string) => string;
+            function logItOut(message: string, f: StringModifier) {
+                console.log(`At ${new Date().toISOString()}: ${f(message)}`);
+            }
+
+            logItOut('Tacos', (s: string) => s.toUpperCase());
+
+            function decorate(x: string) {
+                return `***${x}***`;
+            }
+
+            logItOut('Burrito', decorate);
+        });
+
+        describe('HOF that returns a function', () => {
+
+            it('with just a normal ', () => {
+                // <element>content</element>
+
+                function tagMaker(element: string, content: string) {
+                    return `<${element}>${content}</${element}>`;
+                }
+
+                expect(tagMaker('customer', 'Bob Smith')).toBe('<customer>Bob Smith</customer>');
+                expect(tagMaker('customer', 'Sue Jones')).toBe('<customer>Sue Jones</customer>');
+            });
+
+            it('oop version', () => {
+
+                class TagMaker {
+                    private element: string;
+
+                    constructor(element: string) {
+                        this.element = element;
+                    }
+
+                    make(content: string) {
+                        return `<${this.element}>${content}</${this.element}>`;
+                    }
+
+                }
+
+                const customerMaker = new TagMaker('customer');
+
+                expect(customerMaker.make('Bob Smith')).toBe('<customer>Bob Smith</customer>');
+
+
+            });
+
+            it('how a functional programmer would do it.', () => {
+
+                function tagMaker(element: string) {
+                    return (content: string) => `<${element}>${content}</${element}>`;
+                }
+
+                const customerMaker = tagMaker('customer');
+
+                expect(customerMaker('Bob Smith')).toBe('<customer>Bob Smith</customer>');
+            });
+
+        });
+
     });
 
 });
