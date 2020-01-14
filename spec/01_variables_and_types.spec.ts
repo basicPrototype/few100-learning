069b4e29030396f08efc9cc604a0a83e87ebab59
basicPrototype <rmarquis@gmail.com>
2020-01-14 12:47:13 -0500

    more type and string examples

--- spec/01_variables_and_types.spec.ts
+++ spec/01_variables_and_types.spec.ts
@@ -1,6 +1,6 @@
 describe('declaring variables', () => {
     it('an example', () => {
-        const name = 'bob';
+        const name = 'Bob';
         expect(name).toBe('Bob');
     });
 
@@ -17,6 +17,11 @@ describe('declaring variables', () => {
 
         age = ['a', 'b'];
         // lint complains because this is neither a string or number
+
+        // add any so it doesn't complain.  tslint won't allow functions without type declaration.
+        function add(a: any, b: any) {
+            return a + b;
+        }
     });
 
     it('initializing a variable defines (infers) the type', () => {
@@ -27,4 +32,72 @@ describe('declaring variables', () => {
 
         name = 1138; // lint errors because we just changed the type and it started as an inferred string
     });
+
+    describe('a bit about strings', () => {
+        it('can e delimited with single or double', () => {
+            const name = 'Bob'; // typed as double quote; tslint changed to single
+            // tslint:disable-next-line: quotemark
+            expect(name).toBe("Bob");
+        });
+        it('string literals - interpreted (using backticks)', () => {
+            const name = `Bob`;
+            expect(name).toBe('Bob');
+
+            const story = `some
+            multi
+            line
+            thing`;
+
+            const age = 27;
+
+            // string interpolation
+            const message = 'The name is ' + name + ' and the age is ' + age + '.';
+            const message2 = `The name is ${name} and the age is ${age}.`;
+            expect(message).toBe(message2);
+
+            const newElement = `<div>
+            <h2>${name}</h2>
+            <h2>${age}</h2>
+            </div>`;
+        });
+
+        it('const - be careful', () => {
+            // const doesn't allow you to assign a new value to that *variable*
+            // but it does allow you to assign to an object assigned to that variable
+
+            const x = 12;
+            // x=14; //doesn't allow this
+
+            const favoriteNumbers = [2, 4, 9, 20];
+            favoriteNumbers[2] = 10; // this is *allowed*
+            console.log(favoriteNumbers);
+            expect(favoriteNumbers[2]).toBe(10);
+
+            const movie = {
+                title: 'Rise of Skywalker',
+                yearReleased: 2020
+            };
+
+            movie.yearReleased = 2019; // this is *allowed*
+            expect(movie.yearReleased).toBe(2019);
+
+            // using inerfaces
+            interface Movie {
+                title: string;
+                yearReleased?: number; // ? makes this parameter optional
+            }
+
+            const film1: Movie = {
+                title: '2001'
+            };
+
+            const film2: Movie = {
+                title: '2010',
+                yearReleased: 1999
+            };
+
+        };
+    });
 });
+
+})
