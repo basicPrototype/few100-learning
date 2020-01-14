026705cda371972030a80866c216c131f974f914
basicPrototype <rmarquis@gmail.com>
2020-01-14 10:35:59 -0500

    adding some variable declaration and lint examples

--- spec/01_variables_and_types.spec.ts
+++ spec/01_variables_and_types.spec.ts
@@ -1,6 +1,30 @@
 describe('declaring variables', () => {
     it('an example', () => {
-        let name = 'Bob';
+        const name = 'bob';
         expect(name).toBe('Bob');
+    });
+
+    it('declaring a variable with let', () => {
+        let age: number | string;
+        // age. intellsense shows only methods on both a number anda  string
+        age = 50;
+
+        age = 51;
+        // intellisense only shows number options
+
+        age = 'Old';
+        // intellisense only shows string options
+
+        age = ['a', 'b'];
+        // lint complains because this is neither a string or number
+    });
+
+    it('initializing a variable defines (infers) the type', () => {
+        let name = 'Bob';
+
+        name = 'Steve';
+        name = 'Kara';
+
+        name = 1138; // lint errors because we just changed the type and it started as an inferred string
     });
 });
