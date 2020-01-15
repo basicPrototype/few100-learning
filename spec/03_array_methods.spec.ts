d125ca52cd449d51dbe9d49b56e6332859ddde85
basicPrototype <rmarquis@gmail.com>
2020-01-15 11:08:50 -0500

    at second break

--- spec/03_array_methods.spec.ts
+++ spec/03_array_methods.spec.ts
@@ -20,15 +20,24 @@ describe('array methods', () => {
     });
 
     describe('array methods that create new arrays', () => {
-        it('should behave...', () => {
+        it('map', () => {
 
+            // LINQ: select
             const doubled = numbers.map(n => n + n);
             expect(doubled).toEqual([2, 4, 6, 8, 10, 12, 14, 16, 18]);
+        });
+
+        it('has a filter', () => {
+            // LINQ: where
+            const evens = numbers.filter(isEven);
+            expect(evens).toEqual([2, 4, 6, 8]);
         });
     });
 });
 
 describe('two types of loops that may or may not be', () => {
+    const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
     it('for in loop', () => {
         // const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
         // let total = 0;
@@ -51,7 +60,6 @@ describe('two types of loops that may or may not be', () => {
     // useful in angular templates
     // does what a c# programmer would expect for-in loop to do.
     it('for of loop', () => {
-        const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
         let total = 0;
         for (const n of numbers) {
             total += n;
@@ -59,4 +67,57 @@ describe('two types of loops that may or may not be', () => {
         expect(total).toBe(45);
 
     });
+
+    it('using reduce', () => {
+        let total = numbers.reduce((s, n) => s + n);
+        expect(total).toBe(45);
+        total = numbers.reduce((s, n) => s + n, 100);
+        expect(total).toBe(145);
+    });
 });
+
+// shopping cart.  want to know total price and shipping price
+describe('an example', () => {
+    it('the example', () => {
+        interface CartItem {
+            description: string;
+            qty: number;
+            price: number;
+        }
+
+        const cart: cartItem[] = [
+            { description: 'Eggs', qty: 3, price: 2.37 },
+            { description: 'Bread', qty: 2, price: 3.50 },
+            { description: 'Beer', qty: 6, price: 7.50 },
+        ];
+
+        // now figure out the total quantity and price in the cart
+        interface ShippingInfo {
+            totalQty: number;
+            totalPrice: number;
+        }
+
+        const starter: ShippingInfo = {
+            totalQty: 0,
+            totalPrice: 0
+        };
+        // first parameter determines the return type
+        // created starter so the initial item is a quantity and not a cart item
+        const result = cart.reduce((s: ShippingInfo, n: CartItem) => {
+            return {
+                totalQty: s.totalQty += n.qty,
+                totalPrice: s.totalPrice += n.price
+            };
+        }, starter);
+
+        expect(result.totalQty).toBe(11);
+        expect(result.totalPrice).toBeCloseTo(13.37, 2);
+
+        const roundedPrice: number = result.totalPrice.toFixed(2);
+        expect(roundedPrice).toEqual(13.37);
+
+
+    });
+});
+
+
